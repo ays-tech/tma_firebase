830ce8ec0307c4166cb1351e024c7427fd259df8
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-07-08 08:25:56 +0300

    Make homepage a Hello, @username!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Mini Apps Quest</h1>
+<p>Hello, @{window.Telegram.WebApp.initDataUnsafe.user.username}!</p>
