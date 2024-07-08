7ed375cb87c4bf89f7607ada5b3887c0f6f645a8
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-07-08 09:18:30 +0300

    Center homepage content and use shadcn typography

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Mini Apps Quest</h1>
-<p>Hello, @{window.Telegram.WebApp.initDataUnsafe.user.username}!</p>
+<div class="flex h-screen items-center justify-center">
+	<div class="text-center">
+		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Mini Apps Quest</h1>
+		<p class="leading-7 [&:not(:first-child)]:mt-6">
+			Hello, @{window.Telegram.WebApp.initDataUnsafe.user.username}!
+		</p>
+	</div>
+</div>
