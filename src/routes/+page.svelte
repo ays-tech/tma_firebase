637f44f325d306845149f1c0761bd2187857a271
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-07-08 10:24:55 +0300

    Add retry checks for undefined Telegram.WebApp

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,28 @@
+<script lang="ts">
+	import { onMount } from 'svelte';
+
+	let MiniApp: any;
+
+	const checkMiniApp = () => {
+		if (window.Telegram && window.Telegram.WebApp) {
+			MiniApp = window.Telegram.WebApp;
+		} else {
+			setTimeout(checkMiniApp, 100);
+		}
+	};
+
+	onMount(() => {
+		checkMiniApp();
+	});
+</script>
+
 <div class="flex h-screen items-center justify-center">
 	<div class="text-center">
 		<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Mini Apps Quest</h1>
 		<p class="leading-7 [&:not(:first-child)]:mt-6">
-			Hello, @{window.Telegram.WebApp.initDataUnsafe.user.username}!
+			{#if MiniApp}
+				Hello, @{MiniApp.initDataUnsafe.user.username}!
+			{/if}
 		</p>
 	</div>
 </div>
