9cfd95a0f29d0ddc7bc782d5ce55e5f18b10c8a7
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-07-23 15:33:18 +0300

    Add a NotFound 404 page

---
+++ src/routes/NotFound.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { Button } from '$lib/components/ui/button';
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center gap-3 text-center">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">404</h1>
+	<Button href="/">Go Home</Button>
+</div>
