12f2e1b3a7edd70302198f4611960e9a06e6188c
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-07-23 15:34:31 +0300

    Use SPA Router in App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,6 +1,19 @@
-<script>
+<script lang="ts">
 	import './app.css';
+
+	import Router from 'svelte-spa-router';
+
 	import Home from './routes/Home.svelte';
+	import About from './routes/About.svelte';
+	import NotFound from './routes/NotFound.svelte';
+
+	let routes = {
+		'/': Home,
+		'/about': About,
+		'*': NotFound
+	};
 </script>
 
-<Home />
+<main>
+	<Router {routes} />
+</main>
