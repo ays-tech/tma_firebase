47c044d62d3e8030eb288205971a55b3a64acc9f
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-07-23 15:34:06 +0300

    Add About.svelte page

---
+++ src/routes/About.svelte
@@ -0,0 +1,3 @@
+<div class="flex h-screen flex-col items-center justify-center gap-3 text-center">
+	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">About Mini App</h1>
+</div>
