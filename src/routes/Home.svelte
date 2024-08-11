12ff8dd045b42b13f17c2d3f69dc92b66216076c
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-08-11 14:22:45 +0300

    Use TMA SDK in Home.svelte

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -1,28 +1,12 @@
 <script lang="ts">
 	import { Button } from '$lib/components/ui/button';
-	import { onMount } from 'svelte';
-
-	let MiniApp: any;
-
-	const checkMiniApp = () => {
-		if (window.Telegram?.WebApp) {
-			MiniApp = window.Telegram.WebApp;
-		} else {
-			setTimeout(checkMiniApp, 100);
-		}
-	};
-
-	onMount(() => {
-		checkMiniApp();
-	});
+	import { retrieveLaunchParams } from '@telegram-apps/sdk';
+	const { initData } = retrieveLaunchParams();
 </script>
 
 <div class="flex h-screen flex-col items-center justify-center gap-3 text-center">
 	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Mini Apps Quest</h1>
 	<p class="leading-7">
-		{#if MiniApp}
-			Hello, @{MiniApp.initDataUnsafe.user.username}!
-		{/if}
+		Hello, @{initData?.user?.username}!
 	</p>
-	<Button>Button</Button>
 </div>
