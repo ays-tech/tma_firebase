0454964c05f92de80723d5c299378ccdabf1b234
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-08-11 14:44:24 +0300

    Add back button navigation pop functionality to App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,28 @@
 <script lang="ts">
 	import './app.css';
 
-	import Router from 'svelte-spa-router';
+	import Router, { pop, push } from 'svelte-spa-router';
+
+	import { postEvent, on } from '@telegram-apps/sdk';
 
 	import Home from './routes/Home.svelte';
 	import About from './routes/About.svelte';
 	import NotFound from './routes/NotFound.svelte';
+	import { onMount } from 'svelte';
 
 	let routes = {
 		'/': Home,
 		'/about': About,
 		'*': NotFound
 	};
+
+	onMount(() => {
+		postEvent('web_app_setup_back_button', { is_visible: true });
+
+		on('back_button_pressed', (payload) => {
+			pop();
+		});
+		});
 </script>
 
 <main>
