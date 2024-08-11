cd032815d4566a7b65e04adbc3b212d21b780be8
Ahmed ElBatanony <ahmedelbatanony@gmail.com>
2024-08-11 14:25:07 +0300

    Add util fn to format date as dd.mm.yyyy

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -60,3 +60,12 @@ export const flyAndScale = (
 		easing: cubicOut
 	};
 };
+
+export function formatDate(date: Date | number): string {
+	const d = new Date(date);
+	const day = d.getDate().toString().padStart(2, '0');
+	const month = (d.getMonth() + 1).toString().padStart(2, '0');
+	const year = d.getFullYear();
+
+	return `${day}.${month}.${year}`;
+}
