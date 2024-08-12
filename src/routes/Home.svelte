60695ce27fbc77901762a2bb6d1dbcbb68224a16
ays-tech <54658247+ays-tech@users.noreply.github.com>
2024-08-12 06:00:14 +0000

    new

--- src/routes/Home.svelte
+++ src/routes/Home.svelte
@@ -7,6 +7,6 @@
 <div class="flex h-screen flex-col items-center justify-center gap-3 text-center">
 	<h1 class="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">Mini Apps Quest</h1>
 	<p class="leading-7">
-		Hello, @{initData?.user?.username}! lets figure a way btn
+		Hello, hy ho are you @{initData?.user?.username}! lets figure a way btn
 	</p>
 </div>
